7ace89aa246c3f8370c933429b8fa80efba6fff8
Pragadeeshwaran15 <pragadeshm501@gmail.com>
2023-12-19 11:07:23 +0530

    Smaple portfolio completed

---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+
+function App() {
+  let data=[{
+    title:"Project One",
+    description:`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam
+    aspernatur eum quasi sapiente nesciunt? Voluptatibus sit, repellat sequi itaque deserunt,
+    dolores in, nesciunt, illum tempora ex quae? Nihil, dolorem! `
+  },
+{
+  title:"Project Two",
+    description:`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod
+    odio, gravida pellentesque urna varius vitae.`
+},
+{
+  title:"Project Three",
+    description:`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos quisquam, error
+    quod sed cumque, odio distinctio velit nostrum temporibus necessitatibus et facere atque iure
+    perspiciatis mollitia recusandae vero vel quam!`
+},
+{
+  title:"Project Four",
+    description:`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod
+    odio, gravida pellentesque urna varius vitae.`
+},
+{
+  title:"Project Five",
+    description:`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod
+    odio, gravida pellentesque urna varius vitae.`
+},
+{
+  title:"Project Six",
+  description:`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque earum nostrum
+  suscipit ducimus nihil provident, perferendis rem illo, voluptate atque, sit eius in voluptates,
+  nemo repellat fugiat excepturi! Nemo, esse.`
+}]
+  return <>
+  {/* <!-- Page Content --> */}
+<div className="container">
+
+  {/* <!-- Page Heading --> */}
+  <h1 className="my-4">Page Heading
+    <small>Secondary Text</small>
+  </h1>
+
+  <div className="row">
+    {
+      data.map((e,i)=>{
+       return <div className="col-lg-4 col-sm-6 mb-4" key={i}>
+      <div className="card h-100">
+        <a href="#"><img className="card-img-top" src="https://via.placeholder.com/700x400" alt=""/></a>
+        <div className="card-body">
+          <h4 className="card-title">
+            <a href="#">{e.title}</a>
+          </h4>
+          <p className="card-text">{e.description}</p>
+        </div>
+      </div>
+    </div>
+      }) 
+    }
+  </div>
+ 
+</div>
+{/* <!-- /.container --> */}
+  </>
+}
+
+export default App
